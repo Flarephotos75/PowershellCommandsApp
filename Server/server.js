9f755d08757e3dd4aff269fdd0975e7098103731
Flarephotos75 <steven@flare-photography.com>
2023-07-05 22:01:52 +0100

    Ready for testing on a Windows device for Troublshooting

--- Server/server.js
+++ Server/server.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const { exec } = require('child_process');
+const path = require('path');
 
 const app = express();
 const port = 8000;
 
 // API endpoint to execute PowerShell script
-app.get('/execute-script/', (req, res) => {
-  const scriptPath = '../src/Scripts/';
+app.get('/execute-script', (req, res) => {
+  const scriptName = req.query.scriptName;
+  console.log(scriptName, "*****SCRIPT NAME****");
 
-  exec(`pwsh -ExecutionPolicy Bypass -File ${scriptPath}`, (error, stdout, stderr) => {
+  const scriptPath = path.join('../src/Scripts', scriptName);
+
+  exec(`pwsh -ExecutionPolicy Bypass -File "${scriptPath}"`, (error, stdout, stderr) => {
     if (error) {
-      console.error(`Error executing PowerShell script: ${error}`);
+      console.error(`Error executing PowerShell script: ${error.message}`);
+      console.error(`Standard Error: ${stderr}`);
       res.status(500).json({ error: 'Internal Server Error' });
     } else {
       console.log('PowerShell script executed successfully');
@@ -23,3 +28,33 @@ app.get('/execute-script/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+
+// REDUNDANT CODE - ARCHIVED
+// const express = require('express');
+// const { exec } = require('child_process');
+
+// const app = express();
+// const port = 8000;
+
+// // API endpoint to execute PowerShell script
+// app.get('/execute-script/', (req, res, {scriptName}) => {
+//   console.log(scriptName, "*****SCRIPT NAME****");
+//   const scriptPath = '../src/Scripts/' + {scriptName};
+
+//   exec(`pwsh -ExecutionPolicy Bypass -File ${scriptPath}`, (error, stdout, stderr) => {
+//     if (error) {
+//       console.error(`Error executing PowerShell script: ${error}`);
+//       res.status(500).json({ error: 'Internal Server Error' });
+//     } else {
+//       console.log('PowerShell script executed successfully');
+//       res.json({ result: stdout });
+//     }
+//   });
+// });
+
+// // Start the server
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
+
